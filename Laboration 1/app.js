4e5a5ba8a6870f7ebaca84552ce45344c6d09779
speelgren <97473666+speelgren@users.noreply.github.com>
2022-05-02 10:21:31 +0200

    laboration början

--- Laboration 1/app.js
+++ Laboration 1/app.js
@@ -1,5 +1,10 @@
 //Laboration 1
 
+//npm install -g nodemon
+//npm init
+//npm install express
+//npm install jsdom
 
-
-
+const express = require('express');
+const jsDOM = require('jsdom');
+const fs = require('fs');
