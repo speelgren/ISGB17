e2befb6a3aae9c1b7a8e01f3c50a21594230cd87
speelgren <97473666+speelgren@users.noreply.github.com>
2022-05-21 15:31:50 +0200

    lab2

--- Laboration 2/app.js
+++ Laboration 2/app.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const jsdom = require('jsDOM');
 const cookieParser = require('cookie-parser');
 const io = require('socket.io')(http);
 
@@ -13,53 +12,57 @@ app.use('/public', express.static(__dirname + '/public'));
 
 let server = http.listen(3001, () => {
 
-    console.log('Server is running ' + server.address().port);
+  console.log('Server is running ' + server.address().port);
 });
 
 app.get('/', (request, response) => {
 
-    let cookie = request.cookies.nickName;
+  let cookie = request.cookies.nickName;
 
-    if(cookie == null) {
+  if(cookie == null) {
 
-        response.sendFile(__dirname + '/loggain.html');
-    } else {
+      response.sendFile(__dirname + '/loggain.html');
+  } else {
 
-        response.sendFile(__dirname + '/index.html');
-    }
+      response.sendFile(__dirname + '/index.html');
+  }
 });
 
 app.get('/loggain', (request, response) => {
 
-    response.sendFile(__dirname + '/loggain.html');
+  response.sendFile(__dirname + '/loggain.html');
 });
 
 app.post('/', (request, response) => {
 
-    console.log(request.body.nickname);
+  response.cookie('nickName', request.body.nickname);
+  response.redirect('/');
 
-    response.cookie('nickName', request.body.nickname);
-    response.redirect('/');
-
-    console.log('Cookie set.');
+  console.log('Cookie set.');
 });
 
 app.get('/favicon.ico', (request, response) => {
 
-    response.sendFile(__dirname + '/favicon.ico');
+  response.sendFile(__dirname + '/favicon.ico');
 });
 
 io.sockets.on('connection', (socket) => {
 
-    let cookieString = socket.handshake.headers.cookie;
-    let cookieList = cookieParser.JSONCookies(cookieString);
+  console.log('ny användare ansluten');
 
-    socket.on('newMessage', (data) => {
+  let cookieString = socket.handshake.headers.cookie;
+  let cookieList = cookieParser.JSONCookies(cookieString);
+  let date = new Date().toISOString().split('T')[0];
+  /* split('=')[1] för att ta bort "nickName=" ur cookieList */
+  let cookieName = cookieList.split('=')[1];
 
-        io.emit('message', {
+  socket.on('newMessage', (data) => {
 
-            'name' : cookieList,
-            'newMessage' : data.messageID
-        });
-    });
+      io.emit('message', {
+
+          'date' : date,
+          'name' : cookieName,
+          'newMessage' : data.messageID,
+      });
+  });
 });
--- Laboration 2/public/script/client-script.js
+++ Laboration 2/public/script/client-script.js
@@ -4,69 +4,64 @@ const socket = io();
 
 window.addEventListener('load', () => {
 
-    console.log('DOM LOADED');
+  let sendBTN = document.querySelector('.btn');
+  sendBTN.addEventListener('click', (event) => {
+  event.preventDefault();
+  let sendMSG = document.querySelector('#msg').value;
 
-    let sendBTN = document.querySelector('.btn');
-    sendBTN.addEventListener('click', (event) => {
+    try {
 
-        event.preventDefault();
-        let sendMSG = document.querySelector('#msg').value;
+      /* Validerar längden på meddelandet */
+      if(sendMSG.length < 3) throw new Error ('Meddelandet är för kort');
 
-        try {
+      else {
+          /* emit till app.js socket */
+          socket.emit('newMessage', {
 
-            /* Validerar längden på meddelandet */
-            if(sendMSG.length < 3) throw new Error ('Meddelandet är för kort');
-            
-            else {
-
-                /* emit till app.js socket */
-                socket.emit('newMessage', {
-                    'messageID' : sendMSG
-                });
-            }
-
-                /* Rensare textfältet efter meddelande skickats */
-                document.querySelector('#msg').value = null;
-        } catch(error) {
-
-            alert(error);
-            console.log(error);
+            'messageID' : sendMSG
+          });
         }
 
+        /* Rensare textfältet efter meddelande skickats */
+        document.querySelector('#msg').value = null;
+
+    } catch(error) {
+
+        alert(error);
+        console.log(error);
+      }
     });
 });
 
 /* Mottagning av app.js socket data */
 socket.on('message', (data) => {
 
-    /* Sektion för meddelande */
-    let flowSection = document.querySelector('#flow');
-    /* Ny div för varje meddelande */
-    let newCHAT = document.createElement('div');
+  /* Sektion för meddelande */
+  let flowSection = document.querySelector('#flow');
+  /* Ny div för varje meddelande */
+  let newCHAT = document.createElement('div');
 
-    /* Tid */
-    let date = new Date().toISOString().split('T')[0];
-    let dateEl = document.createElement('h6');
-    let dateNode = document.createTextNode(date);
-    dateEl.style.display = 'inline-block';
-    dateEl.style.marginRight = '5px';
-    dateEl.appendChild(dateNode);
+  /* Tid */
+  let dateEl = document.createElement('h6');
+  let dateNode = document.createTextNode(data.date);
+  dateEl.style.display = 'inline-block';
+  dateEl.style.marginRight = '5px';
+  dateEl.appendChild(dateNode);
 
-    /* Namn */
-    let nameEl = document.createElement('h6');
-    /* Måste kunna göra detta bättre med cookie-parser?? Hur??? */
-    let nameNode = document.createTextNode(data.name.replace('nickName=', '') + ': ');
-    nameEl.style.fontWeight = 'bold';
-    nameEl.style.display = 'inline-block';
-    nameEl.style.marginRight = '5px';
-    nameEl.appendChild(nameNode);
+  /* Namn */
+  let nameEl = document.createElement('h6');
+  let nameNode = document.createTextNode(data.name + ': ');
+  nameEl.style.fontWeight = 'bold';
+  nameEl.style.display = 'inline-block';
+  nameEl.style.marginRight = '5px';
+  nameEl.appendChild(nameNode);
 
-    /* Nytt meddelande */
-    let messageEl = document.createElement('h6');
-    let messageNode = document.createTextNode(data.newMessage);
-    messageEl.style.display = 'inline-block';
-    messageEl.appendChild(messageNode);
+  /* Nytt meddelande */
+  let messageEl = document.createElement('h6');
+  let messageNode = document.createTextNode(data.newMessage);
+  messageEl.style.display = 'inline-block';
+  messageEl.appendChild(messageNode);
 
-    newCHAT.append(dateEl ,nameEl, messageEl);
-    flowSection.appendChild(newCHAT);
+  newCHAT.append(dateEl ,nameEl, messageEl);
+  flowSection.appendChild(newCHAT);
 });
